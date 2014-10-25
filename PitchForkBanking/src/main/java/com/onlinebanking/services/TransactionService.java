9c5e7332438f66c71e83c93f279ec2b73ae7e3b6
pjbhatt1 <pjbhatt1@asu.edu>
2014-10-24 18:27:27 -0700

    Added methods for requests relating to transactions required by employee.

--- PitchForkBanking/src/main/java/com/onlinebanking/services/TransactionService.java
+++ PitchForkBanking/src/main/java/com/onlinebanking/services/TransactionService.java
@@ -1,5 +1,15 @@
 package com.onlinebanking.services;
 
+import java.util.List;
+
+import com.onlinebanking.models.Requests;
+import com.onlinebanking.models.Transaction;
+
 public interface TransactionService {
 
+	public void addRequest(Requests request);
+	public List<Requests> getAllRequestsFromUser(String userId);
+	public List<Transaction> getListOfApprovedTransactionRequests(String userId);
+	public void updateTransaction(Transaction transaction);
+	public void deleteTransaction(Transaction transaction);
 }
